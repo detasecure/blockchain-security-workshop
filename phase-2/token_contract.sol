269a8ae7981f1bb8c9749e2304557327bb4508da
yuva-detasecure <100260812+yuva-detasecure@users.noreply.github.com>
2023-01-19 15:34:31 +0530

    Update token_contract.sol

--- phase-2/token_contract.sol
+++ phase-2/token_contract.sol
@@ -1,17 +1,29 @@
 pragma solidity 0.6.0;
 contract Token {
   mapping(address => uint) balances;
+  mapping (address=>uint256) getBalance;
   uint public totalSupply;
-  function Token(uint _initialSupply) {
-    balances[msg.sender] = totalSupply = _initialSupply;
+  address public minter;
+
+
+  constructor() public {
+        minter=msg.sender;
+    }
+  function token(uint _initialSupply) public {
+    balances[minter] = totalSupply = _initialSupply;
   }
+
+  function deposit_and_mint(address _to, uint256 amount) external {
+        require(msg.sender==minter,"Not an minter");
+        getBalance[_to] += amount;
+    }
   function transfer(address _to, uint _value) public returns (bool) {
     require(balances[msg.sender] - _value >= 0);
     balances[msg.sender] -= _value;
     balances[_to] += _value;
     return true;
   }
-  function balanceOf(address _owner) public constant returns (uint balance) {
+  function balanceOf(address _owner) public view returns (uint balance) {
     return balances[_owner];
   }
 }
